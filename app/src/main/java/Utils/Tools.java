b343a956aa2b99478a7b613119ba038b68c2b23a
wangyy <wangyy1119@thundersoft.com>
2019-01-04 18:31:14 +0800

    0.2

--- app/src/main/java/Utils/Tools.java
+++ app/src/main/java/Utils/Tools.java
@@ -4,8 +4,7 @@ public class Tools {
     public String changeBrace(String str){
         str = str.replace("[","");
         str= str.replace("]","");
-        str= str.replace(" ","");
-//        str = str.replace(" ",",");
+        //str = str.replace(" ",",");
         return str;
     }
 }

d4d5f1d0d9a2ee2ee754c5220049b04d6fd01be2
wangyy <wangyy1119@thundersoft.com>
2019-01-08 16:30:38 +0800

    0.2

--- app/src/main/java/Modle/SdMap.java
+++ app/src/main/java/Modle/SdMap.java
@@ -10,7 +10,7 @@ public class SdMap extends HashMap<String,Integer>{
 	public SdMap(){
 		this.put("烟台",38);
 		this.put("威海",39);
-		this.put("成山角",40);
+		this.put("城山角",40);
 		this.put("石岛",41);
 	}
 }

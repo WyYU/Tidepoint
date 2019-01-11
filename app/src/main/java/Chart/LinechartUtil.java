853e110bfb146f11e65540f249eb970b905e0872
wangyy <wangyy1119@thundersoft.com>
2019-01-11 17:28:26 +0800

    0.2

---
+++ app/src/main/java/Chart/LinechartUtil.java
@@ -0,0 +1,21 @@
+package Chart;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.LineChart;
+
+public class LinechartUtil {
+
+    private boolean DRAGABLE = true;
+
+    private LineChart lineChart;
+    private Layout layout;
+    private Context context;
+
+
+    public LinechartUtil(Context context,Layout layout){
+        
+    }
+}
